d0d2f6d2606c287aeb6b32cb485b6abe1f9fc4bc
Emhope <130700707+Emhope@users.noreply.github.com>
2023-12-05 17:38:48 +0300

    Update mars1.py

--- mars1.py
+++ mars1.py
@@ -1,6 +1,8 @@
+#! /usr/bin/env python3.8
+
 import sys
 import time
-from typing import List
+from typing import List, Dict
 
 import pygame
 import random
@@ -9,7 +11,50 @@ import pickle
 from mars_sim import *
 
 
-def auction(robots: List[Robot], block: Block) -> Tuple[Robot, float]:
+def create_plan(robots: List[Block], blocks: List[Block], goal: GoalPoint) -> Dict[Robot, List[Block]]:
+    plan = {robot: [] for robot in robots}
+    free_blocks = blocks.copy()
+    robots_queue = robots.copy()
+
+    while free_blocks:
+        current_sprint = []
+        for robot in robots_queue:
+            if free_blocks:
+                nearest_block = min(free_blocks, key=lambda b: calcDistance(robot.getPosition(), b.getPosition()))
+                plan[robot].append(nearest_block)
+                free_blocks.remove(nearest_block)
+                current_sprint.append((robot, nearest_block))
+        robots_queue = sorted(current_sprint, key=lambda i: calcDistance(i[1].getPosition(), goal.getPosition()))
+        robots_queue = map(lambda i :i[0], robots_queue)
+    return plan
+
+
+def work_by_plan(plan: Dict[Robot, List[Block]], goal: GoalPoint) -> None:
+    for robot in plan:
+        if robot.target is goal and calcDistance(robot.getPosition(), goal.getPosition()) < EPS_DIST:
+            if robot.attachedObject is not None:
+                plan[robot].remove(robot.attachedObject)
+                robot.attachedObject.finish = True
+                robot.attachedObject = None
+                if plan[robot]:
+                    robot.target = plan[robot][0]
+                    robot.target.reservedRobot = robot
+                else:
+                    robot.target = None
+        
+        elif robot.target is not None and calcDistance(robot.getPosition(), robot.target.getPosition()) < EPS_DIST:
+            robot.takeObject(robot.target)
+            robot.target = goal
+        
+        elif robot.target is None and plan[robot]:
+            robot.target = plan[robot][0]
+            robot.target.reservedRobot = robot
+
+            
+
+        
+
+def auction(robots: List[Robot], block: Block) -> Robot:
     """
     Выбор самого выгодного робота для блока
     """
@@ -25,38 +70,48 @@ def auction(robots: List[Robot], block: Block) -> Tuple[Robot, float]:
     return (best_robot, best_price)
 
 
+
+
 def distributeTasks_auction(robots: List[Robot], blocks: List[Block], goal: Block) -> None:
-    '''
     
-    '''
-    free_robots = [robot for robot in robots if robot.target is None and robot.attachedObject is None]
-    free_blocks = [block for block in blocks if not block.finish and block.reservedRobot is None]
+    for robot in robots:
+        if robot.target is goal:
+            if calcDistance(robot.getPosition(), goal.getPosition()) < EPS_DIST:
+                if robot.attachedObject is not None:
+                    robot.attachedObject.finish = True
+                    robot.attachedObject = None
+                    robot.target = None
+                else:
+                    robot.target = None
+
+        elif robot.target is not None:
+            if calcDistance(robot.getPosition(), robot.target.getPosition()) < EPS_DIST:
+                robot.takeObject(robot.target)
+                robot.target = goal
+        
+
+        if robot.attachedObject and robot.target is None:
+            robot.target = goal
+
+    free_robots = [robot for robot in robots if robot.target is None]
+    free_blocks = [block for block in blocks if block.reservedRobot is None]
+
+
     
     while free_robots:
         if not free_blocks:
             for robot in free_robots:
-                #if calcDistance(robot.getPosition(), goal.getPosition()) > EPS_DIST:
-                robot.target = goal
+                if calcDistance(robot.getPosition(), goal.getPosition()) >= EPS_DIST:
+                    robot.target = goal
                 return
 
         prices = [(auction(free_robots, block), block) for block in free_blocks]
         (best_robot, best_price), best_block = min(prices, key=lambda i: i[0][1])
         best_robot.target = best_block
-        best_robot.takeObject(best_block)
         best_block.reservedRobot = best_robot
-        
+
         free_robots = [robot for robot in robots if robot.target is None and robot.attachedObject is None]
         free_blocks = [block for block in blocks if not block.finish and block.reservedRobot is None]
-    
-    not_free_robots = [robot for robot in robots if not (robot.target is None)]
-    for robot in not_free_robots:
-        if robot.target is goal and calcDistance(robot.getPosition(), goal.getPosition()) <= EPS_DIST:
-            robot.target = None
-            robot.attachedObject = None
-        elif not (robot.target is None) and calcDistance(robot.getPosition(), robot.target.getPosition()) <= EPS_DIST:
-            robot.takeObject(robot.target)
-            robot.target = goal
-
 
 
 
@@ -151,12 +206,12 @@ im creating scene:
     for i in range(robot_num):
         new_pos = create_new_accepted_pos(all_poses=all_poses, min_dist=min_dist, scene_size=scene_size)
         all_poses.append(new_pos)
-        r['robots'].append(Robot(new_pos[0], new_pos[1]))
+        r['robots'].append(Robot(new_pos[0], new_pos[1], name=f'Robot{i+1}'))
 
     for i in range(blocks_num):
         new_pos = create_new_accepted_pos(all_poses=all_poses, min_dist=min_dist, scene_size=scene_size)
         all_poses.append(new_pos)
-        r['blocks'].append(Block(new_pos[0], new_pos[1]))
+        r['blocks'].append(Block(new_pos[0], new_pos[1], name=f'Block{i+1}'))
 
     for i in range(goals):
         new_pos = create_new_accepted_pos(all_poses=all_poses, min_dist=min_dist, scene_size=scene_size)
@@ -177,7 +232,7 @@ def load_scene(scene_name='last_env.pickle'):
 
 
 
-def main(scene_name=None):
+def main(scene_name=None, distribute_method='auction'):
     """
     Главная управляющая функция
 
@@ -205,6 +260,14 @@ def main(scene_name=None):
     # Целевая точка на сцене
     goal = scene['goals'][0]
 
+    if distribute_method == 'static':
+        plan = create_plan(robots=robots, blocks=blocks, goal=goal)
+        for robot in plan:
+            print(f'{robot.name}:')
+            for i, block in enumerate(plan[robot]):
+                print(f'\t{i+1}. {block.name}')
+            print()
+
     start = time.time()
     while True:
         # Проверка заверешения симуляции
@@ -216,8 +279,13 @@ def main(scene_name=None):
         # Основная логика МАРС
         if checkMission(robots, blocks, goal) is True:
             break
-
-        distributeTasks_auction(robots, blocks, goal)
+        
+        if distribute_method == 'auction':
+            distributeTasks_auction(robots, blocks, goal)
+        elif distribute_method == 'static':
+            work_by_plan(plan=plan, goal=goal)
+        else:
+            distributeTasks(robots, blocks, goal)
 
         # Отрисовка объектов
         screen.fill((255, 255, 255))
@@ -234,10 +302,10 @@ def main(scene_name=None):
         pygame.display.update()
         pygame.time.delay(50)
     
-    print(f"Time: {time.time() - start}")
+    print(f"method: {distribute_method}\nTime: {time.time() - start}\n")
 
 
 if __name__ == '__main__':
-    #main(scene_name='last_env.pickle')
-    main()
+    main(scene_name='last_env.pickle', distribute_method='static')
+    main(scene_name='last_env.pickle', distribute_method='auction')
 
